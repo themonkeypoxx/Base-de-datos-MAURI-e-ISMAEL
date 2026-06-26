8262a51e59a9f54b6bf6613dc077ae56d26cdfa7
damonkepoxx <isma.figueroa06@gmail.com>
2026-06-26 11:35:35 -0400

    ReadmeFin

--- test.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-hash_guardado = '$2b$12$YKaSCBME0cyBhGRCiH0AcOIscfuCbzJLsl.px0UCBuVPs/6.BsGfq'
-password_a_probar = "Adm1n_Test!!"
-
-resultado = bcrypt.checkpw(password_a_probar.encode('utf-8'), hash_guardado.encode('utf-8'))
-print(f"¿Coincide?: {resultado}")
